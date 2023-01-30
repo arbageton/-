91b5e7089c0ff0c484db234722900edccda6785a
Матвей Иванов <arbageton13@yandex.ru>
2023-01-30 22:02:39 +0300

    '1'

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+<body> 
+  <div>hello</div>
+  <button id = "increment" variant="contained" color="primary" disableElevation>
+    increment
+  </button>
+
+
+ </body>
+
+)}
+
+
+export default App;
